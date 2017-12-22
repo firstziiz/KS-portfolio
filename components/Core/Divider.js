d33c22382da36b3fbfd673d39f524900c131d017
Kanisorn S <firstziiz.k@gmail.com>
2017-12-22 23:32:46 +0700

    initial KS Portfolio Profile Section

---
+++ components/Core/Divider.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Divider = styled.div`
+  text-align: center;
+  border: 0;
+  margin: 0;
+  padding: 3em 0;
+  &:before {
+    content: '...';
+    display: inline-block;
+    margin-left: 1.4em;
+    color: rgba(0,0,0,.7);
+    position: relative;
+    top: -.5em;
+    font-style: italic;
+    font-size: 1.2em;
+    letter-spacing: 1.4em;
+  }
+`
+
+export default Divider
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { injectGlobal } from 'styled-components'
+
+import Profile from '../components/Profile'
+import Divider from '../components/Core/Divider'
+
+injectGlobal`
+  body {
+    font-size: 12px;
+    letter-spacing: .015em;
+  }
+`
 
 const Index = () => (
-  <div>KS</div>
+  <div>
+    <Profile />
+    <Divider />
+    {/* <Profile /> */}
+  </div>
 )
 
 export default Index
