77a45b515822fc09771edb95d6f6091d093d0884
Kanisorn S <firstziiz.k@gmail.com>
2018-02-02 23:02:36 +0700

    refactor circle.yml

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = +process.env.PORT || 3002
 const outDir = require('path').join(__dirname, 'out')
+
 if (!require('fs').existsSync(outDir)) {
   console.error('Error: "out" folder not found.')
   console.error('Please run "yarn run build" before running this script.')
   console.error()
   process.exit(1)
 }
+
 app.use(require('compression')())
 app.use('/', express.static(outDir))
 
