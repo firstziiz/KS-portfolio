9cde8cb11771f7e062342d734b4cf066280d6df8
Kanisorn S <firstziiz.k@gmail.com>
2018-07-22 14:10:04 +0700

    add favicon

--- static.config.js
+++ static.config.js
@@ -28,6 +28,13 @@ export default {
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <title>{`KS 🌹 Personal Website`}</title>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+            <meta name="msapplication-TileColor" content="#cfdef3" />
+            <meta name="theme-color" content="#cfdef3" />
             <link
               rel="stylesheet"
               href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
