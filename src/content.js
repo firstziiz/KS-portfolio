cfc918e8f19c3037a8413a9ad4f9a3b1fd009618
Kanisorn S <firstziiz.k@gmail.com>
2019-01-25 02:41:00 +0700

    [UPDATE] content and images for my portfolio

--- src/content.js
+++ src/content.js
@@ -1,3 +1,14 @@
+import RepublicImage from './static/working/7republic.png'
+import MarvelicImage from './static/working/marvelic.jpg'
+import ProntoToolsImage from './static/working/prontotools.png'
+import TopvalueImage from './static/working/topvalue.png'
+
+import YWC15Image from './static/projects/ywc15.png'
+import LunaImage from './static/projects/luna.png'
+import SimplesatImage from './static/projects/simplesat.png'
+import KMUTTElectionImage from './static/projects/kmutt_election.png'
+import WIPCamp9Image from './static/projects/wipcamp9.jpg'
+
 const data = {
   profile: {
     position: 'Former Software Engineer Intern',
@@ -13,8 +24,8 @@ const data = {
       time: 'June 2018 – August 2018',
       title: 'Software Engineer Intern',
       location: 'Pronto Tools',
-      locationUrl: '',
-      logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
+      locationUrl: 'https://prontotools.io',
+      logo: ProntoToolsImage,
       description: `&bull; Worked with simplesat.io team to develop the simple satisfaction system.
       &bull; Using React / Django with TDD concept
       &bull; UI Testing with Robot Framework
@@ -26,8 +37,8 @@ const data = {
       time: 'June 2016 – July 2016',
       title: 'Full Stack Developer Intern',
       location: 'Marvelic Engine Co., Ltd',
-      locationUrl: '',
-      logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
+      locationUrl: 'https://www.marvelic.co.th/en/',
+      logo: MarvelicImage,
       description: `&bull; An internet application studio
       &bull; Developed an e-commerce website with Magento and created Magento plugin to use in the production.
       `
@@ -36,8 +47,8 @@ const data = {
       time: 'April 2015 – June 2015',
       title: 'Front-End Developer (Volunteer)',
       location: 'Topvalue Co., Ltd',
-      locationUrl: '',
-      logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
+      locationUrl: 'https://www.topvalue.com/',
+      logo: TopvalueImage,
       description: `&bull; A C2C & B2C e-commerce startup
       &bull; Coded HTML, CSS, Javascript, and some part of PHP
       &bull; Learned about corporation culture`
@@ -46,8 +57,8 @@ const data = {
       time: 'March 2014 – May 2014',
       title: 'Web Developer Intern',
       location: '7republic',
-      locationUrl: '',
-      logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
+      locationUrl: 'https://www.facebook.com/sevenrepublic/',
+      logo: RepublicImage,
       description: `&bull; An internet application studio
       &bull; Coded HTML, CSS, Javascript and developed with front end frameworks such as jQuery.`
     }
@@ -56,16 +67,26 @@ const data = {
     {
       time: '2018',
       location: 'School of Information Technology, KMUTT',
-      locationUrl: '',
+      locationUrl: 'http://sit.kmutt.ac.th',
       title: 'Luna.codes',
       shortDesc: 'Web Application for Improving (Java) Programming Skill',
-      logo:
-        'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+      logo: LunaImage,
       description: `&bull; Inspiration by HackerRank, LeetCode, CodeSignal, CodinGame but focuses on education purpose.
       &bull; Working with teammates in Agile methodology
       &bull; Developing by React, Django, NodeJS, Java and implement on Docker Compose, CI/CD with CircleCI
       `,
-      tags: ['React', 'Redux', 'Django'],
+      tags: [
+        'React',
+        'MobX',
+        'Django',
+        'PostgresDB',
+        'NodeJS',
+        'Javac',
+        'Docker Compose/Swarm',
+        'CircleCI',
+        'Datadog',
+        'Zabbix'
+      ],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
@@ -91,11 +112,19 @@ const data = {
       title: 'Simplesat.io',
       shortDesc:
         'The CSAT/NPS Satisfaction System that can integrate with ConnectWise, Zendesk, and Autotask',
-      logo:
-        'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+      logo: SimplesatImage,
       description: `&bull; Developed by React, Django with Micro-services concept
       &bull; Doing UI testing, Unit test with TDD concept.`,
-      tags: ['React', 'Redux', 'Django'],
+      tags: [
+        'React',
+        'Redux',
+        'Django',
+        'Microservices',
+        'Pytest',
+        'Robot Framework',
+        'PostgresDB',
+        'CircleCI'
+      ],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
@@ -103,29 +132,27 @@ const data = {
     {
       time: '2018',
       location: 'Alchemist',
-      locationUrl: '',
+      locationUrl: 'https://alchemist.itbangmod.in.th',
       title: 'KMUTT Election 2018',
       shortDesc: 'The KMUTT Student Council and Student Organization Election',
-      logo:
-        'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+      logo: KMUTTElectionImage,
       description: `&bull; Developed by React, Laravel, MariaDB with docker load balancing system
       &bull; Developed with cybersecurity awareness according to OWASP Top 10
       `,
-      tags: ['React', 'Redux', 'Django'],
+      tags: ['React', 'Redux', 'Laravel', 'Docker', 'Nginx', 'OWASP TOP TEN', 'Jenkins'],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
     },
     {
       time: '2017',
-      location: 'Thailand Webmaster Association',
-      locationUrl: '',
+      location: 'Thai Webmaster Association',
+      locationUrl: 'https://www.webmaster.or.th/',
       title: 'Young Webmaster Camp 15th Website',
       shortDesc: '',
-      logo:
-        'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+      logo: YWC15Image,
       description: `&bull; Developed by React with NextJS Framework for Server-side Rendering`,
-      tags: ['React', 'Redux', 'Django'],
+      tags: ['React', 'Redux', 'NodeJS', 'MongoDB', 'Docker'],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
@@ -145,15 +172,14 @@ const data = {
     //   mediumUrl: ''
     // },
     {
-      time: '2016 - 2017',
+      time: '2016 -',
       location: 'School of Information Technology, KMUTT',
-      locationUrl: '',
+      locationUrl: 'http://sit.kmutt.ac.th',
       title: 'WIP Camp 8th and 9th — Web Application and Tools',
       shortDesc: '',
-      logo:
-        'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+      logo: WIPCamp9Image,
       description: `&bull; Developed a registration system with Laravel and the camp landing web page with Greensock, jQuery`,
-      tags: ['React', 'Redux', 'Django'],
+      tags: ['jQuery', 'Greensock', 'Laravel', 'MariaDB', 'CentOS'],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
