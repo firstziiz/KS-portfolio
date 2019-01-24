4c46dcb14b460132027ea6115af170a811f8d946
Kanisorn S <firstziiz.k@gmail.com>
2019-01-25 02:07:44 +0700

    [UPDATE] content in web portfolio

--- src/content.js
+++ src/content.js
@@ -1,113 +1,191 @@
 const data = {
+  profile: {
+    position: 'Former Software Engineer Intern',
+    company_link: 'https://www.prontotools.io/',
+    company_name: 'Pronto Tools'
+  },
   now: {
-    list: ['a', 'a', 'a', 'a', 'a', 'a'],
+    list: ['13231', 'a', 'a', 'a', 'a', 'a'],
     updated: 'Jul 2018'
   },
   experiences: [
     {
-      time: '2017 - Current',
-      title: 'Front-End Developer (Volunteer)',
-      location: 'Young Passionate Program',
+      time: 'June 2018 – August 2018',
+      title: 'Software Engineer Intern',
+      location: 'Pronto Tools',
       locationUrl: '',
       logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
-      description: `Lorem <a href='ipsum' target='_blank'>ipsum</a>, 
-      dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! 
-      Perspiciatis facilis quas voluptate? Quam eius commodi veritatis laboriosam necessitatibus 
-      nulla facere quaerat non, maiores ipsam?`
+      description: `&bull; Worked with simplesat.io team to develop the simple satisfaction system.
+      &bull; Using React / Django with TDD concept
+      &bull; UI Testing with Robot Framework
+      &bull; Using CI/CD with CircleCI.
+      &bull; Learned and following Agile methodology from certificated scrum master and product owner.
+      `
     },
     {
-      time: '2017 - Current',
-      title: 'Front-End Developer (Volunteer)',
-      location: 'Young Passionate Program',
+      time: 'June 2016 – July 2016',
+      title: 'Full Stack Developer Intern',
+      location: 'Marvelic Engine Co., Ltd',
       locationUrl: '',
       logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
-      description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate? Quam eius commodi veritatis laboriosam necessitatibus nulla facere quaerat non, maiores ipsam?`
+      description: `&bull; An internet application studio
+      &bull; Developed an e-commerce website with Magento and created Magento plugin to use in the production.
+      `
     },
     {
-      time: '2017 - Current',
+      time: 'April 2015 – June 2015',
       title: 'Front-End Developer (Volunteer)',
-      location: 'Young Passionate Program',
+      location: 'Topvalue Co., Ltd',
       locationUrl: '',
       logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
-      description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate? Quam eius commodi veritatis laboriosam necessitatibus nulla facere quaerat non, maiores ipsam?`
+      description: `&bull; A C2C & B2C e-commerce startup
+      &bull; Coded HTML, CSS, Javascript, and some part of PHP
+      &bull; Learned about corporation culture`
     },
     {
-      time: '2017 - Current',
-      title: 'Front-End Developer (Volunteer)',
-      location: 'Young Passionate Program',
+      time: 'March 2014 – May 2014',
+      title: 'Web Developer Intern',
+      location: '7republic',
       locationUrl: '',
       logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
-      description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate? Quam eius commodi veritatis laboriosam necessitatibus nulla facere quaerat non, maiores ipsam?`
-    },
-    {
-      time: '2017 - 2018',
-      title: 'Front-End Developer (Volunteer)',
-      location: 'Young Passionate Program',
-      locationUrl: '',
-      logo: 'https://abinavseelan.com/static/hashnode.7956f957.jpg',
-      description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate? Quam eius commodi veritatis laboriosam necessitatibus nulla facere quaerat non, maiores ipsam?`
+      description: `&bull; An internet application studio
+      &bull; Coded HTML, CSS, Javascript and developed with front end frameworks such as jQuery.`
     }
   ],
   projects: [
     {
-      time: 'July 2017',
-      location: 'Pronto Tools',
+      time: '2018',
+      location: 'School of Information Technology, KMUTT',
       locationUrl: '',
-      title: 'Simplesat',
-      shortDesc: 'Simple Satisfaction for human',
+      title: 'Luna.codes',
+      shortDesc: 'Web Application for Improving (Java) Programming Skill',
       logo:
         'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
-      description: `Lorem <a href='ipsum' target='_blank'>ipsum</a>, 
-        dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! 
-        Perspiciatis facilis quas voluptate? Quam eius commodi veritatis laboriosam necessitatibus 
-        nulla facere quaerat non, maiores ipsam?`,
+      description: `&bull; Inspiration by HackerRank, LeetCode, CodeSignal, CodinGame but focuses on education purpose.
+      &bull; Working with teammates in Agile methodology
+      &bull; Developing by React, Django, NodeJS, Java and implement on Docker Compose, CI/CD with CircleCI
+      `,
+      tags: ['React', 'Redux', 'Django'],
+      githubUrl: '',
+      projectUrl: '',
+      mediumUrl: ''
+    },
+    // {
+    //   time: 'July 2017',
+    //   location: 'School of Information Technology, KMUTT',
+    //   locationUrl: '',
+    //   title: 'SIT Connect 2018',
+    //   shortDesc: 'The KMUTT Student Council and Student Organization Election',
+    //   logo:
+    //     'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+    //   description: `...`,
+    //   tags: ['React', 'Redux', 'Django'],
+    //   githubUrl: '',
+    //   projectUrl: '',
+    //   mediumUrl: ''
+    // },
+    {
+      time: '2018',
+      location: 'Pronto Tools',
+      locationUrl: '',
+      title: 'Simplesat.io',
+      shortDesc:
+        'The CSAT/NPS Satisfaction System that can integrate with ConnectWise, Zendesk, and Autotask',
+      logo:
+        'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+      description: `&bull; Developed by React, Django with Micro-services concept
+      &bull; Doing UI testing, Unit test with TDD concept.`,
       tags: ['React', 'Redux', 'Django'],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
     },
     {
-      time: 'July 2017',
-      location: 'Pronto Tools',
+      time: '2018',
+      location: 'Alchemist',
       locationUrl: '',
-      title: 'Simplesat',
-      shortDesc: 'Simple Satisfaction for human',
+      title: 'KMUTT Election 2018',
+      shortDesc: 'The KMUTT Student Council and Student Organization Election',
       logo:
         'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
-      description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate?'`,
+      description: `&bull; Developed by React, Laravel, MariaDB with docker load balancing system
+      &bull; Developed with cybersecurity awareness according to OWASP Top 10
+      `,
       tags: ['React', 'Redux', 'Django'],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
     },
     {
-      time: 'July 2017',
-      location: 'Pronto Tools',
+      time: '2017',
+      location: 'Thailand Webmaster Association',
       locationUrl: '',
-      title: 'Simplesat',
-      shortDesc: 'Simple Satisfaction for human',
+      title: 'Young Webmaster Camp 15th Website',
+      shortDesc: '',
       logo:
         'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
-      description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate?'`,
+      description: `&bull; Developed by React with NextJS Framework for Server-side Rendering`,
       tags: ['React', 'Redux', 'Django'],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
     },
+    // {
+    //   time: 'July 2017',
+    //   location: 'Alchemist',
+    //   locationUrl: '',
+    //   title: 'KMUTT Club 2018',
+    //   shortDesc: '',
+    //   logo:
+    //     'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+    //   description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate?'`,
+    //   tags: ['React', 'Redux', 'Django'],
+    //   githubUrl: '',
+    //   projectUrl: '',
+    //   mediumUrl: ''
+    // },
     {
-      time: 'July 2017',
-      location: 'Pronto Tools',
+      time: '2016 - 2017',
+      location: 'School of Information Technology, KMUTT',
       locationUrl: '',
-      title: 'Simplesat',
-      shortDesc: 'Simple Satisfaction for human',
+      title: 'WIP Camp 8th and 9th — Web Application and Tools',
+      shortDesc: '',
       logo:
         'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
-      description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate?'`,
+      description: `&bull; Developed a registration system with Laravel and the camp landing web page with Greensock, jQuery`,
       tags: ['React', 'Redux', 'Django'],
       githubUrl: '',
       projectUrl: '',
       mediumUrl: ''
     }
+    // {
+    //   time: 'July 2017',
+    //   location: 'Alchemist',
+    //   locationUrl: '',
+    //   title: 'SIT CRAFT CAMP Website',
+    //   shortDesc: '',
+    //   logo:
+    //     'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+    //   description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate?'`,
+    //   tags: ['React', 'Redux', 'Django'],
+    //   githubUrl: '',
+    //   projectUrl: '',
+    //   mediumUrl: ''
+    // },
+    // {
+    //   time: 'July 2017',
+    //   location: 'KMUTT',
+    //   locationUrl: '',
+    //   title: 'TEDxKMUTT Landing Website',
+    //   shortDesc: '',
+    //   logo:
+    //     'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1476930694/his8lxs1d2ljks4jyzd0.png',
+    //   description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet ipsa nam unde amet consequuntur! Perspiciatis facilis quas voluptate?'`,
+    //   tags: ['React', 'Redux', 'Django'],
+    //   githubUrl: '',
+    //   projectUrl: '',
+    //   mediumUrl: ''
+    // }
   ]
 }
 
